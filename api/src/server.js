cab2e0d133b94113e1994d7741aecc05a5a08331
Juan Carlos Aragón <juank1316@gmail.com>
2021-05-15 00:17:05 -0600

    Se corrigio error en el url de la base de datos

--- api/src/server.js
+++ api/src/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const routes = require('./routes/routes');
 const mongoose = require('mongoose');
-const port = 3000;
+const port = 3002;
 
 
 const app = express();
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const mongodbURL = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}}`
+const mongodbURL = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
 mongoose.connect(mongodbURL, {useNewUrlParser: true, useUnifiedTopology: true})
 	.then(db => console.log('DB is conencted to', db.connection.host))
 	.catch(err => {console.log("###################ESTE SERIA EL ERROR#######################"); console.error(err);});
